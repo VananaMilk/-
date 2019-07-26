69c67b3479ed536796e17d17155d61a69cfaef3d
VananaMilk <53331980+VananaMilk@users.noreply.github.com>
2019-07-26 21:01:07 +0900

    Add files via upload

---
+++ python basic 1장.py
@@ -0,0 +1,90 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('hello world!')
+hello world!
+>>> Print('hello world!')
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    Print('hello world!')
+NameError: name 'Print' is not defined
+>>> print('3.14')
+3.14
+>>> print(1)
+1
+>>> print(999)
+999
+>>> hello world!
+SyntaxError: invalid syntax
+>>> 3
+3
+>>> print(3-2)
+1
+>>> print(4/2)
+2.0
+>>> print(5**2)
+25
+>>> print(5//3)
+1
+>>> print(9999//3)
+3333
+>>> print(999%5)
+4
+>>> print(999//5)
+199
+>>> print(3+7)
+10
+>>> print(6*3)
+18
+>>> print(4**2)
+16
+>>> print(9%5)
+4
+>>> print('3+2')
+3+2
+>>> pritn('토끼'+'야 안녕')
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    pritn('토끼'+'야 안녕')
+NameError: name 'pritn' is not defined
+>>> print('야'+'안녕')
+야안녕
+>>> print('fuck'*4)
+fuckfuckfuckfuck
+>>> A='fuck'
+>>> print(A*3)
+fuckfuckfuck
+>>> B=you
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    B=you
+NameError: name 'you' is not defined
+>>> B='YOU'
+>>> print(A+B)
+fuckYOU
+>>> count='fuck'
+>>> print(count)
+fuck
+>>> count=count*3
+>>> print(count)
+fuckfuckfuck
+>>> my_name = "Rho Hyun Woo'
+SyntaxError: EOL while scanning string literal
+>>> my_name='Rho Hyun Woo'
+>>> print(my_name)
+Rho Hyun Woo
+>>> name='노현우'
+>>> print(name)
+노현우
+>>> coffee=4100
+>>> tea=3900
+>>> juice=4600
+>>> print(coffee*3 + juice*4 + tea*1)
+34600
+>>> print(coffee*5 + tea*1 + juice*4)
+42800
+>>> print(coffee*6 + tea*7)
+51900
+>>> tea = 4100
+>>> coffee = 5600 # 커피의 가격
+>>> juice = 6100 # 쥬스의 가격
+>>> 
