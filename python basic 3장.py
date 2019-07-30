7e3025a301f6791114dc3bda62a204ce08db922b
VananaMilk <53331980+VananaMilk@users.noreply.github.com>
2019-07-30 21:06:19 +0900

    Add files via upload

---
+++ python basic 3장.py
@@ -0,0 +1,180 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Rho/AppData/Local/Programs/Python/Python37-32/practice.py 
+['수페코', '수캬루', '노조미', '마코토']
+['수페코', '수캬루', '노조미', '마코토']
+['수페코', '수캬루', '노조미', '마코토']
+['수페코', '수캬루', '노조미', '마코토']
+>>> 
+ RESTART: C:/Users/Rho/AppData/Local/Programs/Python/Python37-32/practice.py 
+수페코
+수캬루
+노조미
+마코토
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수페코 나와라!
+수캬루 나와라!
+노조미 나와라!
+마코토 나와라!
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수페코 나와라!
+수캬루 나와라!
+노조미 나와라!
+마코토 나와라!
+프
+린
+세
+스
+커
+넥
+트
+ 
+리
+다
+이
+브
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수페코
+['수페코', '수캬루', '사렌', '모니카']
+수캬루
+['수페코', '수캬루', '사렌', '모니카']
+사렌
+['수페코', '수캬루', '사렌', '모니카']
+모니카
+['수페코', '수캬루', '사렌', '모니카']
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+0
+1
+2
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+1
+2
+3
+4
+5
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+Traceback (most recent call last):
+  File "C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py", line 1, in <module>
+    for list in range(가,하):
+NameError: name '가' is not defined
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+9 x 1 = 2
+9 x 2 = 4
+9 x 3 = 6
+9 x 4 = 8
+9 x 5 = 10
+9 x 6 = 12
+9 x 7 = 14
+9 x 8 = 16
+9 x 9 = 18
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+9 x 1 = 9
+9 x 2 = 18
+9 x 3 = 27
+9 x 4 = 36
+9 x 5 = 45
+9 x 6 = 54
+9 x 7 = 63
+9 x 8 = 72
+9 x 9 = 81
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+['수영복', '수영복', '수영복']
+>>> characters=['페코','캬루','코코로']
+for i in range(3):
+    characters[i]='수영복'
+
+print(characters)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수페코
+수캬루
+수코로
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수페코
+수캬루
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+['수페코', '수캬루', '수코로']
+['수페코', '수캬루', '수코로']
+['수페코', '수캬루', '수코로']
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+['수페코', '수캬루', '수코로'] 뽑혀라!
+['수페코', '수캬루', '수코로'] 뽑혀라!
+['수페코', '수캬루', '수코로'] 뽑혀라!
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수페코 뽑혀라!
+수캬루 뽑혀라!
+수코로 뽑혀라!
+>>> 
+ RESTART: C:\Users\Rho\AppData\Local\Programs\Python\Python37-32\practice.py 
+수
+페
+코
+>>> for character in '수페코':
+	print(character)
+
+수
+페
+코
+>>> characters=['수페코','수캬루']
+>>> for character in characters:
+	print(character, '나와라')
+
+	
+수페코 나와라
+수캬루 나와라
+>>> for letter in '프린세스 커넥트':
+	print(letter)
+
+	
+프
+린
+세
+스
+ 
+커
+넥
+트
+>>> roses=['하얀장미','하얀장미','하얀장미']
+>>> for i in range(3):
+	roses[i]='빨간장미'
+print(roses)
+SyntaxError: invalid syntax
+>>> print(roses)
+['하얀장미', '하얀장미', '하얀장미']
+>>> for i in range(3):
+	roses(i)='빨간장미'
+	
+SyntaxError: can't assign to function call
+>>> for i in range(3)
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> roses=['하얀장미','하얀장미','하얀장미']
+>>> for o in range(3):
+	roses[o]='빨간장미'
+print(roses)
+SyntaxError: invalid syntax
+>>> for i in range(3):
+	roses[i]='빨간장미'
+    print(roses)
+    
+SyntaxError: unindent does not match any outer indentation level
+>>> 
+ RESTART: C:/Users/Rho/AppData/Local/Programs/Python/Python37-32/practice.py 
+['빨간장미', '빨간장미', '빨간장미']
+>>> 
